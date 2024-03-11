dd9089f22e26fb84f2c73cec1e63ff7bf8ea7b8b
ankushsde <ankushs5252@gmail.com>
2024-03-11 19:29:34 +0530

    Redux setup

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-     <>
-        <h1 className="text-xl font-bold underline">
+    //  <Provider store={store}>
+      <div>
+      <h1 className="text-xl font-bold underline">
       Hello world!
     </h1>
-     </>
+      </div>
+      
+    //  </Provider>
   )
 }
 
